33491f98072e5e4beb5a537436edef604dfd0c6f
JiaxingQiu <jiaxingq1996@gmail.com>
2026-04-09 12:10:30 -0400

    upload to blob

--- revlm_dc/revlm_dc/settings.py
+++ revlm_dc/revlm_dc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i+r4yt_54k-s$j^wd)n$^5pray4#s74s1^p_rfi(##v286+u51
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
